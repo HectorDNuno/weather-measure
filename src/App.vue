9cf3f3c4e7dca2803ec655833fa172262c3a94c0
Hector Nuno <hectornuno1994@gmail.com>
2023-09-29 13:12:11 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import './assets/variables.css'
+</script>
+
+<style lang="css">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto', sans-serif;
+}
+</style>
