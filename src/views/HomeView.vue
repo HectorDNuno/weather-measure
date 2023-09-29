cf7f0caa5d2d9e3036b6aa6b1c2bb4000bd8f6e0
Hector Nuno <hectornuno1994@gmail.com>
2023-09-29 13:25:43 -0700

    Add import for components and create navbar
    - creates navbar for entire page
    - imports WeatherInput and WeatherData components
    - adds a container class for those components so they're side by side
    - adds some simple stying for the navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,33 @@
-<script setup></script>
-
 <template>
-  <main></main>
+  <main>
+    <nav class="nav">
+      <h1>Local Weather</h1>
+    </nav>
+
+    <div class="container">
+      <WeatherInput />
+      <WeatherData />
+    </div>
+  </main>
 </template>
+
+<script setup>
+import WeatherInput from '../components/WeatherInput.vue'
+import WeatherData from '../components/WeatherData.vue'
+</script>
+
+<style lang="css">
+.container {
+  display: flex;
+  gap: 35px;
+  padding: 30px;
+}
+
+.nav {
+  background: #5372f0;
+  font-size: 1.3rem;
+  text-align: center;
+  padding: 18px 0;
+  color: #fff;
+}
+</style>
