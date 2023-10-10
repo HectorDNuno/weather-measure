3afe4278a44998fbe385283830e69e613c84aa8f
Hector Nuno <hectornuno1994@gmail.com>
2023-10-09 18:35:03 -0700

    Adjust the way data is displayed on each card
    - receives the data object as a prop
    - creates a weekDays array
    - creates a function to get the weekdays and their
      date for each card
    - rounds each number value
    - updates dot notation to incorporate new data
      object

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -1,43 +1,55 @@
 <template>
-  <ul class="weather-cards">
-    <li class="card">
-      <h3>( ______ )</h3>
-      <img />
-      <h6>Temp: __&deg;F</h6>
-      <h6>Wind: __ mph</h6>
-      <h6>Humidity: __%</h6>
-    </li>
-  </ul>
+  <div class="card">
+    <h3>
+      {{ getDay(data.date) }}
+      {{ new Date(data.date * 1000).getDate() }}
+    </h3>
+
+    <img :src="`http://openweathermap.org/img/wn/${data.img}@2x.png`" alt="" />
+
+    <h4>Day: {{ Math.round(data.day) }}&deg;F</h4>
+    <h4>Night: {{ Math.round(data.night) }}&deg;F</h4>
+    <h4>Rain: {{ data.rain ? Math.round(data.rain) : 0 }}%</h4>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { toRefs } from 'vue';
+
+const props = defineProps({
+  data: {
+    type: Object
+  }
+});
+
+const { data } = toRefs(props);
+
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const getDay = (date) => {
+  const day = new Date(date * 1000).getDay();
+
+  return weekDays[day];
+};
+</script>
 
 <style lang="css" scoped>
-.weather-cards {
-  /* width: calc(100% / 5); */
-}
-.weather-cards .card {
+.card {
   color: #fff;
   background: #6c757d;
-  list-style: none;
-  /* width: calc(100% / 5); */
   border: 3px solid #000;
   border-radius: 10px;
   box-shadow: 5px 5px 0px #000;
   padding: 1rem;
+  text-align: center;
 }
-.weather-cards .card h3 {
-  font-size: 1.3rem;
-  font-weight: 600;
-}
-.weather-cards .card h6 {
-  margin-top: 12px;
-  font-size: 1rem;
+
+.card h4 {
+  margin-top: 5px;
   font-weight: 500;
   letter-spacing: 2px;
 }
-.weather-cards .card img {
-  max-width: 70px;
-  margin: 5px 0 -12px 0;
+.card img {
+  max-width: 4.3rem;
 }
 </style>
