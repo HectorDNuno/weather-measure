f6a7d0ef21909698bb8646fcaeaf914fb7f117d8
Hector Nuno <hectornuno1994@gmail.com>
2023-10-13 16:53:53 -0700

    Remove unused div and update class names

--- src/components/WeatherData.vue
+++ src/components/WeatherData.vue
@@ -1,49 +1,47 @@
 <template>
   <div class="weather-data-container">
-    <div class="current-weather">
-      <div v-if="city && weather" class="weather-info">
-        <div class="location">
-          <div>
-            <h2>
-              {{ city.name + ',' }} {{ city.region === city.name ? city.country : city.region }}
-            </h2>
-            <h2>
-              {{ city.country }}
-            </h2>
-          </div>
-
-          <i class="fa-solid fa-circle-plus fa-2x"></i>
+    <div v-if="city && weather" class="weather-info">
+      <div class="location">
+        <div>
+          <h2>
+            {{ city.name + ',' }} {{ city.region === city.name ? city.country : city.region }}
+          </h2>
+          <h2>
+            {{ city.country }}
+          </h2>
         </div>
 
-        <div class="weather-details">
-          <div>
-            <h6>
-              {{
-                new Date(weather.currentTime).toLocaleDateString('en-us', {
-                  weekday: 'short',
-                  day: '2-digit',
-                  month: 'short'
-                })
-              }}
-              {{ updateTime() }}
-            </h6>
-            <h6>Temperature: {{ Math.round(weather.current.temp) }}&deg;F</h6>
-            <h6>Humidity: {{ weather.current.humidity }}%</h6>
-          </div>
+        <i class="fa-solid fa-circle-plus fa-2x"></i>
+      </div>
 
-          <div>
-            <h6>High: {{ Math.round(weather.daily[0].temp.max) }}&deg;F</h6>
-            <h6>Low: {{ Math.round(weather.daily[0].temp.min) }}&deg;F</h6>
-            <h6>Wind: {{ Math.round(weather.current.wind_speed) }}mph</h6>
-          </div>
+      <div class="weather-details">
+        <div>
+          <h6>
+            {{
+              new Date(weather.currentTime).toLocaleDateString('en-us', {
+                weekday: 'short',
+                day: '2-digit',
+                month: 'short'
+              })
+            }}
+            {{ updateTime() }}
+          </h6>
+          <h6>Temperature: {{ Math.round(weather.current.temp) }}&deg;F</h6>
+          <h6>Humidity: {{ weather.current.humidity }}%</h6>
+        </div>
 
-          <div class="weather-image">
-            <img
-              :src="`http://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`"
-              alt=""
-            />
-            <p>{{ weather.current.weather[0].description }}</p>
-          </div>
+        <div>
+          <h6>High: {{ Math.round(weather.daily[0].temp.max) }}&deg;F</h6>
+          <h6>Low: {{ Math.round(weather.daily[0].temp.min) }}&deg;F</h6>
+          <h6>Wind: {{ Math.round(weather.current.wind_speed) }}mph</h6>
+        </div>
+
+        <div class="weather-image">
+          <img
+            :src="`http://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`"
+            alt=""
+          />
+          <p>{{ weather.current.weather[0].description }}</p>
         </div>
       </div>
     </div>
@@ -86,8 +84,6 @@ const updateTime = () => {
 
 <style lang="css" scoped>
 .weather-data-container {
-}
-.weather-data-container .current-weather {
   border: 3px solid #000;
   border-radius: 10px;
   box-shadow: 5px 5px 0px #000;
@@ -101,13 +97,13 @@ const updateTime = () => {
   display: flex;
   justify-content: space-between;
 }
-.current-weather h6 {
+.weather-data-container h6 {
   margin-top: 12px;
   font-size: 1rem;
   font-weight: 500;
   letter-spacing: 2px;
 }
-.current-weather h2 {
+.weather-data-container h2 {
   font-weight: 700;
   font-size: 1.7rem;
   letter-spacing: 2px;
