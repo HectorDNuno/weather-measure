5fe70a0c76f92202bd7eefc92a7d7f887f9d0422
Hector Nuno <hectornuno1994@gmail.com>
2024-01-19 14:29:49 -0800

    renames backend directory  to api for vercel

---
+++ api/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const qs = require('qs');
+const axios = require('axios');
+const port = 3000;
+
+const weatherAPIUrl = 'https://api.openweathermap.org/data/2.5/onecall';
+const weatherAPIKey = process.env.WEATHER_API_KEY;
+
+app.get('/', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.setHeader('Content-Type', 'text/html');
+
+  let query = req.query;
+  query.appid = weatherAPIKey;
+  let queryString = qs.stringify(query);
+
+  axios(`${weatherAPIUrl}?${queryString}`).then((response) => {
+    res.send(response.data);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+module.exports = app;
