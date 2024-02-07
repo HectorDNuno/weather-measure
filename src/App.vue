08baa0fd4237ba6936b11d1a5ffdc7308e915086
Hector Nuno <hectornuno1994@gmail.com>
2024-02-06 23:01:05 -0800

    updates css reset

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,15 @@ import NavigationBar from './components/NavigationBar.vue';
 </script>
 
 <style lang="css">
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
 * {
   margin: 0;
   padding: 0;
-  box-sizing: border-box;
-  font-family: 'Public Sans', sans-serif;
 }
 
 .app {
@@ -23,12 +27,35 @@ import NavigationBar from './components/NavigationBar.vue';
 }
 
 html {
-  overflow: hidden;
-  height: 100%;
+  background-color: #cee2fd;
 }
 
 body {
   overflow: auto;
   height: 100%;
+  min-height: 100svh;
+  font-family: 'Public Sans', sans-serif;
+}
+
+img,
+picture,
+svg,
+video {
+  display: block;
+  max-width: 100%;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  text-wrap: balance;
+}
+
+p {
+  max-width: 75ch;
+  text-wrap: pretty;
 }
 </style>
