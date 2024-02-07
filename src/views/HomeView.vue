64907e38d06d49c20dc0f9beb97df2e78b39e835
Hector Nuno <hectornuno1994@gmail.com>
2024-02-06 23:21:22 -0800

    Updates components and css
    - removes the nav div and put it in it's own component file
    - removes container div the surrounded the WeatherInput and SavedCities
      components
    - updates css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,28 @@
 <template>
-  <main>
-    <nav class="nav">
-      <h1>Local Weather</h1>
-    </nav>
-
-    <div class="container">
-      <WeatherInput />
-      <WeatherData />
-    </div>
+  <main class="main">
+    <WeatherInput />
+    <SavedCities />
   </main>
 </template>
 
 <script setup>
-import WeatherInput from '../components/WeatherInput.vue'
-import WeatherData from '../components/WeatherData.vue'
+import WeatherInput from '../components/WeatherInput.vue';
+import SavedCities from '../components/SavedCities.vue';
 </script>
 
-<style lang="css">
-.container {
-  display: flex;
-  gap: 35px;
-  padding: 30px;
+<style lang="css" scoped>
+.main {
+  padding: 2rem;
 }
 
-.nav {
-  background: #5372f0;
-  font-size: 1.3rem;
-  text-align: center;
-  padding: 18px 0;
-  color: #fff;
+@media (max-width: 1265px) {
+  .main {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .saved-cities {
+    padding: 0;
+  }
 }
 </style>
