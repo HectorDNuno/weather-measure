f1088cb053a53a4c73012b35593ab66dabe12374
Hector Nuno <hectornuno1994@gmail.com>
2024-02-06 23:04:33 -0800

    Adds ability to save citites in local storage
    - creates two main functions: removeCity and getCityWeatherData
    - removeCity removes the saved city from local storage using via the
      global state
    - getCityWeatherData gets the weather for each saved city in local
      storage. it maps over each city and then updates the city object with
      its own weather
    - uses the onMounted function to check the weather of the saved citites
      each time the user visits the web app
    - updates css class names
    - changes saved-cities container to a grid
    - changes margin and letter spacing of the title class

--- src/components/SavedCities.vue
+++ src/components/SavedCities.vue
@@ -1,20 +1,72 @@
 <template>
-  <div class="current-weather">
-    <div>
-      <h2>City Name</h2>
-      <h6>Temp: __&deg;F</h6>
-      <h6>H: __&deg;F</h6>
-      <h6>L: __&deg;F</h6>
+  <h2 class="title">Saved Cities</h2>
+  <div class="saved-cities">
+    <div v-for="city in sharedState.savedCities" :key="city.id" class="city-weather">
+      <div>
+        <h2>{{ city.city }}</h2>
+        <h6>Temp: {{ Math.round(city.weather.currentTemp) }}&deg;F</h6>
+        <h6>H: {{ Math.round(city.weather.highTemp) }}&deg;F</h6>
+        <h6>L: {{ Math.round(city.weather.lowTemp) }}&deg;F</h6>
+      </div>
+      <i @click="removeCity(city.id)" class="fa-solid fa-circle-minus fa-2x"></i>
     </div>
-
-    <i class="fa-solid fa-circle-minus fa-2x"></i>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted, watchEffect } from 'vue';
+import { sharedState } from '../state';
+import axios from 'axios';
+
+const url = 'https://local-weather-backend.vercel.app/';
+
+const removeCity = (id) => {
+  const index = sharedState.savedCities.findIndex((city) => city.id === id);
+
+  if (index !== -1) {
+    sharedState.savedCities.splice(index, 1);
+  }
+};
+
+const getCityWeatherData = () => {
+  try {
+    watchEffect(() => {
+      sharedState.savedCities.map(async (city, index) => {
+        const latitude = city.coords.lat;
+        const longitude = city.coords.lon;
+
+        const cityWeatherData = await axios.get(
+          `${url}?lat=${latitude}&lon=${longitude}&exclude={part}&units=imperial`
+        );
+
+        sharedState.savedCities[index].weather = {
+          currentTemp: cityWeatherData.data.current.temp,
+          highTemp: cityWeatherData.data.daily[0].temp.max,
+          lowTemp: cityWeatherData.data.daily[0].temp.min
+        };
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+onMounted(() => {
+  getCityWeatherData();
+});
+</script>
 
 <style lang="css" scoped>
-.current-weather {
+.saved-cities {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 2rem;
+}
+.title {
+  margin: 1rem 0;
+  letter-spacing: 2px;
+}
+.city-weather {
   display: flex;
   justify-content: space-between;
   border: 3px solid #000;
@@ -22,25 +74,25 @@
   box-shadow: 5px 5px 0px #000;
   padding: 1rem;
   color: #000;
-  background: #aac7fe;
+  background: #c6b9ff;
 }
-.current-weather h6 {
+.city-weather h6 {
   margin-top: 12px;
   font-size: 1rem;
   font-weight: 500;
   letter-spacing: 2px;
 }
 
-.current-weather p {
+.city-weather p {
   letter-spacing: 2px;
 }
-.current-weather h2 {
+.city-weather h2 {
   font-weight: 700;
   font-size: 1.7rem;
   letter-spacing: 2px;
 }
 
-.current-weather i {
+.city-weather i {
   height: min-content;
   color: #fff;
   background: #000;
@@ -50,7 +102,7 @@
   cursor: pointer;
 }
 
-.current-weather i:hover {
-  color: #6c757d;
+.city-weather i:hover {
+  color: #ff4911;
 }
 </style>
