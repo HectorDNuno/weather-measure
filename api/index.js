58c2dcc2310c0f16a23780497b4246d0f29d2a18
Hector Nuno <hectornuno1994@gmail.com>
2024-02-07 19:26:28 -0800

    Changes axios calls to async functions
    - rewrites routes so that they are async functions instead
    - changes PORT variable so that it's more flexible with vercel
    - uses the params object instead of stringifying the params
    - removes module export line

--- api/index.js
+++ api/index.js
@@ -1,70 +1,60 @@
-const port = 3000;
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const axios = require('axios');
 require('dotenv').config();
-const qs = require('qs');
 
 const weatherAPIUrl = 'https://api.openweathermap.org/data/2.5/onecall';
 const weatherAPIKey = process.env.WEATHER_API_KEY;
 const geoAPIKey = process.env.GEO_API_KEY;
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173'
+  })
+);
 
 app.get('/', (req, res) => {
-  res.json('hi');
+  res.json('local weather backend');
 });
 
-app.get('/search', (req, res) => {
-  const geoApiOptions = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': geoAPIKey,
-      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-    }
-  };
+app.get('/search', async (req, res) => {
+  try {
+    const geoApiOptions = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': geoAPIKey,
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+      }
+    };
+    const geoAPIUrl = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
 
-  const geoAPIUrl = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+    const response = await axios.get(geoAPIUrl, { params: req.query, ...geoApiOptions });
 
-  let query = req.query;
-  let queryString = qs.stringify(query);
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
-  axios(`${geoAPIUrl}?${queryString}`, geoApiOptions)
-    .then((response) => {
-      res.json(response.data);
-    })
-    .catch((error) => {
-      console.error(error);
+// /search?namePrefix=seattle&sort=name
+
+app.get('/weather', async (req, res) => {
+  try {
+    const response = await axios.get(weatherAPIUrl, {
+      params: { ...req.query, appid: weatherAPIKey }
     });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
-app.get('/weather', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.setHeader('Content-Type', 'text/html');
-
-  let query = req.query;
-  query.appid = weatherAPIKey;
-  let queryString = qs.stringify(query);
-
-  axios(`${weatherAPIUrl}?${queryString}`)
-    .then((response) => {
-      res.json(response.data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+// /weather?lat=47.60621&lon=-122.33207&exlude={part}&units=imperial
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-
-module.exports = app;
